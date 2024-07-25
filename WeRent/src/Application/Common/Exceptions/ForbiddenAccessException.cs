7d94e42af198497871462938e22ca10119105ef7
amir elkassar <amir.elkassar@gmail.com>
2024-07-25 18:28:52 +0300

    Add project files.

---
+++ WeRent/src/Application/Common/Exceptions/ForbiddenAccessException.cs
@@ -0,0 +1,6 @@
+﻿namespace WeRent.Application.Common.Exceptions;
+
+public class ForbiddenAccessException : Exception
+{
+    public ForbiddenAccessException() : base() { }
+}
---
+++ WeRent/src/Domain/GlobalUsings.cs
@@ -0,0 +1,2 @@
+﻿global using WeRent.Domain.Common;
+global using WeRent.Domain.Exceptions;
