10eec6467ccc88512e38b89a588f93f28f32bdff
amir elkassar <amir.elkassar@gmail.com>
2024-08-10 21:48:25 +0300

    Admin layout max width added

--- WeRent/src/Web/ClientApp/src/app/[locale]/admin/layout.tsx
+++ WeRent/src/Web/ClientApp/src/app/[locale]/admin/layout.tsx
@@ -4,25 +4,28 @@ import Nav from "@/src/components/nav";
 import Footer from "@/src/components/footer";
 
 interface layoutProps {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }
 function layout({ children }: layoutProps) {
-  return (
-    <div
-      className="min-h-[100vh] "
-      style={{
-        background:
-          "linear-gradient(180deg, rgba(240,246,251,1) 10%,rgb(236, 247, 223) 40%,  rgb(255, 255, 255)  50%, rgb(255, 255, 255) 100%)",
-      }}
-    >
-      <Nav />
-      <main className="px-16 min-h-[calc(100vh-150px)]">
-        <NavLinks />
-        {children}
-      </main>
-      <Footer />
-    </div>
-  );
+    return (
+        <div
+            className="min-h-[100vh] "
+            style={{
+                background:
+                    "linear-gradient(180deg, rgba(240,246,251,1) 10%,rgb(236, 247, 223) 40%,  rgb(255, 255, 255)  50%, rgb(255, 255, 255) 100%)",
+            }}
+        >
+            <div className="max-w-screen-2xl mx-auto">
+                <Nav />
+                <main className="px-16 min-h-[calc(100vh-150px)] ">
+                    <NavLinks />
+                    {children}
+                </main>
+                <Footer />
+            </div>
+           
+        </div>
+    );
 }
 
 export default layout;
