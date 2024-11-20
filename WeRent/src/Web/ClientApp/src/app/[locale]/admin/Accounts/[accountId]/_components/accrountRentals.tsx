0755191feffab68fa239f26106ba9873947f8264
ibrahem-hassan2000 <hibrahem226@gmail.com>
2024-11-20 17:11:08 +0200

    add page profile in admin

--- WeRent/src/Web/ClientApp/src/app/[locale]/admin/Accounts/[accountId]/_components/accrountRentals.tsx
+++ WeRent/src/Web/ClientApp/src/app/[locale]/admin/Accounts/[accountId]/_components/accrountRentals.tsx
@@ -1,38 +1,21 @@
-import ArrowWhiteIcon from "@/src/assets/icons/arrowWhite";
-import EditIcon from "@/src/assets/icons/edit";
-import Button from "@/src/components/button";
-
 import React from "react";
-
 import { Rentals } from "@/src/lib/dataUser";
 import CardRentals from "@/src/components/cardRentals";
-interface AccrountRentalsProps {
-  edit:boolean
-}
-function AccrountRentals({edit}:AccrountRentalsProps) {
+
+function AccrountRentals({}) {
   return (
-    <div className="bg-grayBack pt-20 pb-14 relative before:content-[''] before:w-[calc(100%+130px)] before:bg-grayBack before:absolute before:bottom-0 before:-translate-x-1/2   before:h-full before:left-[50%]">
+    <div className=" mt-20 pb-14 relative ">
       <div className="flex items-center justify-between gap-4 mb-5 relative z-[1]">
         <div className="flex items-center gap-4 mb-6">
-          <h2 className="headTitle">Ahmed Mohamed Rentals</h2>
-          <button className="flex items-center gap-3 bottom-0 bg-transparent">
-            <p className="text-grayMedium text-[20px] font-Medium">Edit</p>
-            <EditIcon className="w-5" />
-          </button>
+          <h2 className="headTitle">Mark Rentals</h2>
         </div>
-        <Button className={"h-10 gap-3"}>
-          <p className="text-white text-[16px] font-Medium">
-          View all rentals
-          </p>
-          <ArrowWhiteIcon />
-        </Button>
+
+        <p className="text-white text-[16px] font-Medium">View all</p>
       </div>
       <div className="flex gap-8 flex-wrap relative z-[1]">
-        {
-          Rentals.map((item)=>{
-            return (<CardRentals edit={edit} data={item} key={item.id}/>)
-          })
-        }
+        {Rentals.map((item) => {
+          return <CardRentals data={item} key={item.id} />;
+        })}
       </div>
     </div>
   );
--- WeRent/src/Web/ClientApp/src/app/[locale]/admin/Accounts/[accountId]/page.tsx
+++ WeRent/src/Web/ClientApp/src/app/[locale]/admin/Accounts/[accountId]/page.tsx
@@ -1,95 +1,27 @@
 "use client";
-import EditIcon from "@/src/assets/icons/edit";
+import EmpoweringImg from "@/src/assets/images/Empowering.png";
 import React from "react";
-import AccountReviews from "./_components/accountReviews";
-import AccountDetails from "./_components/accountDetails";
-import phone from "@/src/assets/images/phone.png";
-import { columns } from "./_components/column";
 import AccrountRentals from "./_components/accrountRentals";
-import { DataTable } from "@/src/components/data-table";
-import { Link } from "@/src/navigation";
-import ROUTES from "@/src/routes";
-import { useSearchParams } from "next/navigation";
 import HeaderEditAcc from "./_components/HeaderEditAcc";
-import AccountEdit from "./_components/accountEdit";
-const dataActivities = [
-  {
-    id: 1,
-    product: "Iphone 15 Pro",
-    productType: "Electronics",
-    activitieType: "Rented Item",
-    user: "Mohamed Ahmed",
-    payment: 100,
-    paymentStatus: "Pending",
-    img: phone,
-    quantity: 50,
-  },
-  {
-    id: 2,
-    product: "Iphone 15 Pro",
-    productType: "Electronics",
-    activitieType: "Leased Item",
-    user: "Amir Elkassar",
-    payment: 250,
-    paymentStatus: "Completed",
-    img: phone,
-    quantity: 50,
-  },
-  {
-    id: 3,
-    product: "Iphone 15 Pro",
-    productType: "Electronics",
+import Image from "next/image";
+import ViewProfile from "@/src/components/viewProfile";
 
-    activitieType: "Rented Item",
-    user: "Karim Fathy",
-    payment: 100,
-    paymentStatus: "Pending",
-    img: phone,
-    quantity: 50,
-  },
-];
 function Page({ params }: any) {
-  const searchParams = useSearchParams();
-
-
   return (
     <div>
-      {searchParams.get("edit") ? (
-        <>
-          <HeaderEditAcc id={params.accountId}/>
-          <AccountEdit />
-        </>
-      ) : (
-        <div>
-          <div className="flex items-center justify-between gap-4 mb-6">
-            <h2 className="headTitle">Account Details</h2>
-            <Link
-              href={
-                ROUTES.ADMIN.ACCOUNTSDETAILS(params.accountId) + "?edit=true"
-              }
-              className="flex items-center gap-3 bottom-0 bg-transparent"
-            >
-              <p className="text-grayMedium text-[20px] font-Medium">
-                Edit profile Details
-              </p>
-              <EditIcon className="w-5" />
-            </Link>
-          </div>
-          <AccountDetails />
-        </div>
-      )}
-
-      <AccountReviews edit={searchParams.get("edit") ? true : false} />
-      <div className="py-16">
-        <DataTable
-          title="Recent Activities"
-          data={dataActivities}
-          viewAll="View all activities"
-          columns={columns}
-          viewAllTitle="View all activities"
+      <HeaderEditAcc id={params.accountId} />
+      <div className="w-full rounded-3xl overflow-hidden h-[180px] lg:h-[356px]">
+        <Image
+          src={EmpoweringImg}
+          alt="Empowering"
+          className="w-full h-full"
+          width={1471.48}
+          height={356.18}
         />
       </div>
-      <AccrountRentals edit={searchParams.get("edit") ? true : false} />
+      <ViewProfile />
+
+      <AccrountRentals />
     </div>
   );
 }
