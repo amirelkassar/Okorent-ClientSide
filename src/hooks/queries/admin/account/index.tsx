1e56aff78809f3f3576fcf1edf5244be663d9f4f
ibrahem-hassan2000 <hibrahem226@gmail.com>
2025-01-11 17:41:19 +0200

    handel pagination in admin account

--- src/hooks/queries/admin/account/index.tsx
+++ src/hooks/queries/admin/account/index.tsx
@@ -1,13 +1,14 @@
+'use client'
 import { api } from "@/src/api/axios";
 import { admin } from "@/src/api/user";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 export const initialQueryKey = "Accounts";
 
 // Get Accounts
-export const GetAccounts = (queries?: any) => {
+export const GetAccounts = (queries:any) => {
   const queryClient = useQueryClient();
   return useQuery({
-    queryKey: [initialQueryKey],
+    queryKey: [initialQueryKey,queries],
     queryFn: async () => {
       const response = await api.get(admin.Accounts.base(queries));
       return response.data;
