1d4ba9d2eb6e702ec1c5eb2b7a9c6354377b6502
ibrahem-hassan2000 <hibrahem226@gmail.com>
2025-01-11 19:25:49 +0200

    handel edit user

--- src/app/[locale]/admin/Accounts/_components/EditModal.tsx
+++ src/app/[locale]/admin/Accounts/_components/EditModal.tsx
@@ -1,3 +1,4 @@
+import React, { useCallback, useState } from "react";
 import Button from "@/src/components/button";
 import Input from "@/src/components/input";
 import InputPhone from "@/src/components/inputPhone";
@@ -5,7 +6,8 @@ import ModalComp from "@/src/components/modal-comp";
 import SelectInput from "@/src/components/select-input";
 import { useEditAccountInAdmin } from "@/src/hooks/queries/admin/account";
 import { Accordion } from "@mantine/core";
-import React from "react";
+import { Toast } from "@/src/components/toast";
+import GetErrorMsg from "@/src/components/getErrorMsg";
 
 function EditModal({
   opened,
@@ -16,93 +18,142 @@ function EditModal({
   opened: boolean;
   close: any;
 }) {
-  const  {mutateAsync:EditUserDetails}=useEditAccountInAdmin();
-  
+  const {
+    mutateAsync: EditUserDetails,
+    error,
+    reset,
+    isError,
+  } = useEditAccountInAdmin();
+
+  // Initialize state with default values from dataUSer
+  const [formState, setFormState] = useState({
+    id: dataUSer.id,
+    name: dataUSer.name || "",
+    email: dataUSer.userName || "",
+    phoneNumber: dataUSer.phoneNumber || "",
+    oldPassword: "",
+    newPassword: "",
+    profileImageFile: "",
+    avatarImageFile: "",
+    address: dataUSer.address || "",
+    city: dataUSer.city || "",
+    country: dataUSer.country || "",
+    zipCode: dataUSer.zipCode || "",
+    region: dataUSer.region || "",
+    plan: "Basic",
+  });
+  console.log(formState);
+
+  // Handle input changes
+  const handleChange = (key: string, value: string) => {
+    if (isError) {
+      reset();
+    }
+    setFormState((prevState) => ({
+      ...prevState,
+      [key]: value,
+    }));
+  };
+
+  const handleSubmit = useCallback(async () => {
+    Toast.Promise(EditUserDetails(formState), {
+      success: "Update User Done",
+      onSuccess: async (res) => {
+        close();
+      },
+    });
+  }, [EditUserDetails, formState]);
   return (
     <ModalComp opened={opened} close={close} title={"Account Details"}>
       <div className="lg:w-[580px] w-full flex flex-col gap-4">
         <div className="flex gap-3 flex-wrap lg:gap-7 w-full">
           <Input
             sectionType="user"
-            defaultValue={dataUSer.name}
+            value={formState.name}
+            onChange={(e) => handleChange("name", e.target.value)}
             inputClassName="bg-white h-12 lg:h-16 rounded-xl"
             label="Name"
             placeholder="Write customer name here"
             className="flex-1"
+            error={GetErrorMsg(error, "Name")}
           />
           <Input
             sectionType="email"
             type="email"
-            defaultValue={dataUSer.userName}
+            value={formState.email}
+            onChange={(e) => handleChange("email", e.target.value)}
             inputClassName="bg-white h-12 lg:h-16 rounded-xl"
-            label="Name"
+            label="Email"
             placeholder="Write customer email here"
             className="flex-1"
+            error={GetErrorMsg(error, "Email")}
           />
         </div>
         <div className="flex gap-3 flex-wrap lg:gap-7 w-full">
           <SelectInput
             data={["Basic", "Basic2", "Basic3", "Basic4", "Basic5"]}
             label="Plan"
-            defaultValue={"Basic"}
+            value={formState.plan}
+            onChange={(value: any) => handleChange("plan", value)}
             className="flex-1"
           />
           <InputPhone
-            value={dataUSer.phoneNumber}
+            value={formState.phoneNumber}
+            onChange={(value) => handleChange("phoneNumber", value)}
             boxClassName={"flex-1"}
             inputClassName="bg-white h-12 lg:h-16 !border border-green/30"
             flagBorder={false}
+            error={GetErrorMsg(error, "PhoneNumber")}
           />
         </div>
         <div>
-          <Accordion
-            transitionDuration={300}
-            className="flex flex-col gap-6"
-            onChange={(e) => {
-              console.log(e);
-            }}
-          >
+          <Accordion transitionDuration={300} className="flex flex-col gap-6">
             <Accordion.Item
               value={"edit"}
               className="border-none bg-blueLight/50 rounded-2xl "
             >
-              <Accordion.Control className=" hover:bg-blueLight/50 h-12 lg:h-16 text-base lg:text-xl font-Medium duration-200 !border border-solid border-green/30 rounded-2xl shadow-sm">
+              <Accordion.Control className="hover:bg-blueLight/50 h-12 lg:h-16 text-base lg:text-xl font-Medium duration-200 !border border-solid border-green/30 rounded-2xl shadow-sm">
                 Address Info
               </Accordion.Control>
-              <Accordion.Panel className=" border-green/30 border border-t-0 pt-6 lg:pt-8   relative -mt-4 border-solid rounded-b-2xl shadow-md ">
+              <Accordion.Panel className="border-green/30 border border-t-0 pt-6 lg:pt-8 relative -mt-4 border-solid rounded-b-2xl shadow-md">
                 <div className="w-full flex flex-col gap-2">
                   <Input
-                    label="Adresses"
+                    label="Address"
+                    value={formState.address}
+                    onChange={(e) => handleChange("address", e.target.value)}
                     inputClassName="bg-white h-12 lg:h-16 rounded-xl"
                     className="flex-1"
-                    defaultValue={dataUSer.address}
                   />
                   <div className="flex gap-3 flex-wrap lg:gap-7">
                     <Input
                       label="Zip Code"
+                      value={formState.zipCode}
+                      onChange={(e) => handleChange("zipCode", e.target.value)}
                       inputClassName="bg-white h-12 lg:h-16 rounded-xl"
                       className="flex-1 min-w-[200px]"
-                      defaultValue={dataUSer.zipCode}
                     />
                     <Input
                       label="City"
+                      value={formState.city}
+                      onChange={(e) => handleChange("city", e.target.value)}
                       inputClassName="bg-white h-12 lg:h-16 rounded-xl"
                       className="flex-1 min-w-[200px]"
-                      defaultValue={dataUSer.city}
                     />
                   </div>
                   <div className="flex gap-3 flex-wrap lg:gap-7">
                     <Input
                       label="Region"
+                      value={formState.region}
+                      onChange={(e) => handleChange("region", e.target.value)}
                       inputClassName="bg-white h-12 lg:h-16 rounded-xl"
                       className="flex-1 min-w-[200px]"
-                      defaultValue={dataUSer.region}
                     />
                     <Input
                       label="Country"
+                      value={formState.country}
+                      onChange={(e) => handleChange("country", e.target.value)}
                       inputClassName="bg-white h-12 lg:h-16 rounded-xl"
                       className="flex-1 min-w-[200px]"
-                      defaultValue={dataUSer.country}
                     />
                   </div>
                 </div>
@@ -113,16 +164,11 @@ function EditModal({
         <div className="flex items-center gap-7 w-full">
           <Button
             onClick={close}
-            className={" flex-1 h-[54px] text-black bg-grayBack border-none"}
+            className={"flex-1 h-[54px] text-black bg-grayBack border-none"}
           >
             Cancel
           </Button>
-          <Button
-            onClick={() => {
-              close();
-            }}
-            className={" flex-1 h-[54px]"}
-          >
+          <Button onClick={handleSubmit} className={"flex-1 h-[54px]"}>
             Save
           </Button>
         </div>
